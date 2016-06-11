1e766b799a189901734f1f6abb2454cc968f3256
Kushal Pandya <kushalspandya@gmail.com>
2016-06-11 16:50:10 +0530

    Main router, all routes now live in seperate folders and files.

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+/**
+ * Poster Server v0.1.0
+ *
+ * A Light-weight ExpressJS REST server for Poster app, it consumes TMDb public APIs.
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 11 June, 2015
+ *
+ * Poster REST Routes.
+ */
+
+var posterApp = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    router;
+
+router = posterApp.Router();
+router.use(bodyParser.urlencoded({ 'extended': 'true' }));
+router.use(bodyParser.json());
+router.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+router.use(methodOverride());
+
+router.use('/movie', require('./movie'));
+
+module.exports = router;
