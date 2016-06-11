3780be4d5574528d0e6450240150a78c4bbae92e
Kushal Pandya <kushalspandya@gmail.com>
2016-06-11 11:11:29 +0530

    Poster Server Starting Script.

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Poster Server v0.1.0
+ *
+ * A Light-weight ExpressJS REST server for Poster app, it consumes TMDb public APIs.
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 11 June, 2015
+ *
+ * Poster Server Script.
+ */
